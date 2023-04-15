0e12582f2dd207568ee8126fb6c3c64f3430b9f7
Lucas <lucas.cgoncalves7@gmail.com>
2023-04-15 14:58:43 -0300

    Adicionar arquivos de projeto.

---
+++ Percorrendo um array com for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Percorrendo_um_array_com_for
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            for(int i = 0; i < num.Length; i++)
+            {
+                num[i] = i;
+                Console.WriteLine("O valor do array e i: {0}= {1} ",num[i],i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
